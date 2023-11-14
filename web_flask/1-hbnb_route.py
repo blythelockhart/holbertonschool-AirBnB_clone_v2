2e03d9dba6bc9a544632a9e4d2482f1ab6592a96
Blythe Lockhart <blythe.lockhart@att.net>
2023-11-14 02:47:32 -0600

    coded file 1-hbnb_route.py

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""
+Starts a Flask web application listening on 0.0.0.0, port 5000.
+The root route ("/") displays the message "Hello HBNB!".
+The root route ("/hbnb") displays “HBNB”.
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return 'Hello HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def display_hbnb():
+    return 'HBNB'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
