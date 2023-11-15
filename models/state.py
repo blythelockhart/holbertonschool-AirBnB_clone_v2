03e29b60d8e69c7c79cc2f7272c23e3b31dcbc68
Blythe Lockhart <blythe.lockhart@att.net>
2023-11-14 20:19:41 -0600

    updated file db_storage and state to correct session issues

--- models/state.py
+++ models/state.py
@@ -11,16 +11,18 @@ from models import storage
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state",
-                          cascade="all, delete-orphan")
+    if storage_type == 'db':
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state",
+                              cascade="all, delete-orphan")
+    else:
+        @property
+        def cities(self):
+            """ Getter attribute for cities in FileStorage. """
+            city_list = []
+            cities = storage.all(City)
 
-    @property
-    def cities(self):
-        """ Getter attribute for cities in FileStorage. """
-        city_list = []
-
-        for city in storage.all(City).values():
-            if city.state_id == self.id:
-                city_list.append(city)
-        return city_list
+            for city in cities.values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
