dd9f9e3848a474ee1ee03485a84747115d831d98
Blythe Lockhart <blythe.lockhart@att.net>
2023-11-14 20:31:10 -0600

    coded files 7 and templates/7

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+""" Starts a Flask web application. """
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """ Display a list of states. """
+    states = storage.all(State).values()
+    sorted_states = sorted(states, key=lambda state: state.name)
+    return render_template('8-states_list.html', states=sorted_states)
+
+
+@app.teardown_appcontext
+def teardown(exception):
+    """ Close the current SQLAlchemy Session. """
+    storage.close()
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
